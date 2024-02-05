bb5daab37de1ab0741a1dc6893ed87db9f6ce003
LionShareZoki <pujic.zoran@gmail.com>
2024-02-05 20:06:37 +0100

    solve eight task

---
+++ 8.js
@@ -0,0 +1,7 @@
+let n = 100;
+let parZbroj = n + 1;
+let brojParova = n / 2;
+
+let zbroj = brojParova * parZbroj;
+
+console.log("Zbroj prvih 100 prirodnih brojeva koristeći Gauss-ovu metodu je:", zbroj);
